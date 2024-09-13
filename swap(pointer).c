cbad9af68745c98e3726a25e1e559463d5fa14ee
Tejaswini-2305 <tejutejaswini235@gmail.com>
2024-09-13 16:16:03 +0530

    Add files via upload

---
+++ swap(pointer).c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void swap(int *x,int *y){
+  int temp = *x;
+  *x = *y;
+  *y = temp;
+  
+}int main(){
+  int a = 10,b = 20;
+  printf("Before swap a = %d, b = %d\n",a,b);
+  swap(&a,&b);
+  printf("After swap a = %d, b= %d\n",a,b);
+  return 0;
+
+  
+}
